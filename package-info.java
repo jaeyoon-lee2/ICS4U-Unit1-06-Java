ba9a8a92ad388f331aacef76c66bf1999090bde1
Jay Lee <jaeyoon.lee@mths.ca>
2021-04-28 23:53:07 +0000

    Create RollDice.java

---
+++ package-info.java
@@ -0,0 +1,9 @@
+/**
+* This program gets the range of random number,
+* generates and shows random number between 1 to max value using procedure.
+*
+* @author  Jay Lee
+* @version 1.0
+* @since   2020-04-28
+*/
+package ca.mths.unit1.unit06.java.main;
